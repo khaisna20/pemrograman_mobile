d00159c3d95328e2d9621d2eb7f613a56dcde6b7
unknown <bobwatcherx@gmail.com>
2024-05-15 09:21:44 +0700

    321

---
+++ lib/ui/produk_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'produk_form.dart'; // Pastikan path ke file produk_form.dart sudah benar
+import './produk_detail.dart';
+
+
+
+
+
+
+class ProdukPage extends StatefulWidget {
+  const ProdukPage({Key? key}) : super(key: key);
+
+  @override
+  _ProdukPageState createState() => _ProdukPageState();
+}
+
+class _ProdukPageState extends State<ProdukPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Data Produk"),
+        actions: [
+          GestureDetector(
+            // menampilkan icon +
+            child: const Icon(Icons.add),
+            //pada saat icon + di tap
+            onTap: () async {
+              //berpindah ke halaman ProdukForm
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ProdukForm(), // Hapus const jika ProdukForm tidak memiliki parameter
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: ListView(
+        children: const [
+        // List 1
+        ItemProduk(
+        kodeProduk: "A001",
+        namaProduk: "Kulkas",
+        harga: 2500000,
+        ),
+        // List 2
+        ItemProduk(
+        kodeProduk: "A002",
+        namaProduk: "TV",
+        harga: 5000000,
+        ),
+        // List 3
+        ItemProduk(
+        kodeProduk: "A003",
+        namaProduk: "Mesin Cuci",
+        harga: 1500000,
+        ),
+        ],
+        ),
+    );
+  }
+}
+
+
+class ItemProduk extends StatelessWidget {
+     final String? kodeProduk;
+     final String? namaProduk;
+     final int? harga;
+
+     const ItemProduk({Key? key, this.kodeProduk, this.namaProduk, this.harga})
+     : super(key: key);
+
+     @override
+     Widget build(BuildContext context) {
+     return GestureDetector(
+      child:Card(
+         child: ListTile(
+         title: Text(namaProduk.toString()),
+         subtitle: Text(harga.toString()),
+         ),
+       ),
+      onTap: () {
+         Navigator.push(
+         context,
+         MaterialPageRoute(
+         builder: (context) => ProdukDetail(
+         kodeProduk: kodeProduk,
+        namaProduk: namaProduk,
+        harga: harga,
+         )));
+ },
+
+      );
+     }
+} 
